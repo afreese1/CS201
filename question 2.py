039ac3808ead07cf8b1b48907416e881eede091e
Alyssa Freese <alyssafreese@gmail.com>
2018-09-21 15:12:14 -0400

    Update question 2.py

--- question 2.py
+++ question 2.py
@@ -1,6 +1,9 @@
 '''("Authors: Freese, Stockla, DePeza")
  ("CS 201, 9/8/2014")
- ("Lab2, Question #2, Temperature conversion")'''
+ ("Lab2, Question #2, Temperature conversion")
+ 
+ converts fahrenheit to celsius and kelvin
+ '''
 
 fah = int(input("Enter the temperature in Fahrenheit"))
 cel = (fah-32)*(9/5)
