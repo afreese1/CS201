45bbc6cae5274b1b5b57986cb0b19ab32e3a4228
Alyssa Freese <alyssafreese@gmail.com>
2018-09-21 15:16:00 -0400

    Update question 3.py

--- question 3.py
+++ question 3.py
@@ -1,6 +1,10 @@
 '''("Authors: Freese, Stockla, DePeza")
  ("CS 201, 9/8/2014")
- ("Lab2, Question #3, conversion")'''
+ ("Lab2, Question #3, conversion")
+ 
+ Converts gallons quarts, pints and cups
+ can be updated to include ounces or to work the other way cups->gal
+ '''
 
 gal = int(input("Enter number of gallons"))
 quart = gal*4
